078fe654f116b7504e4b3537eb13a4e8a8ab58f0
nakkiKujo <joonas.kurki@helsinki.fi>
2016-12-19 15:50:01 +0200

    Update TehtavaController.java

--- src/main/java/wad/controller/TehtavaController.java
+++ src/main/java/wad/controller/TehtavaController.java
@@ -1,6 +1,7 @@
 
 package wad.controller;
 
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -65,30 +66,4 @@ public class TehtavaController {
         return "redirect:/tehtava/" + id;
     }
     
-    @RequestMapping(value = "/tehtava/{id}/palaute", method = RequestMethod.POST)
-    public String kasittelePalaute(@PathVariable String id, Model model, @RequestParam String palauteTeksti) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        Kayttaja kayttaja = kayttajaRepo.findByName(auth.getName());
-        Tehtava tehtava = tehtavaRepo.findOne(id);
-        Palaute palaute = new Palaute();
-        palaute.setPalaute(palauteTeksti);
-        palaute.setPalautteenAntaja(kayttaja);
-        palaute.setPalautteenKohde(tehtava);
-        palauteRepo.save(palaute);      
-        return "redirect:/tehtava/" + id;
-    }
-    
-    @RequestMapping(value="/palautteet", method = RequestMethod.GET)
-    public String naytaPalautteet(Model madonna) {
-        madonna.addAttribute("tehtavat", tehtavaRepo.findAll());
-        return "palauteindex";
-    }
-    
-    @RequestMapping(value="/tehtava/{id}/palaute", method = RequestMethod.GET)
-    public String yhdenTehtavanPalautteet () {
-        
-        
-        
-        return "tehtavanpalautteet";
-    }
 }
